9062472653799752178fe52af95de7711957000d
vassildinev <vassildinev@gmail.com>
2015-10-04 08:32:54 +0300

    Refactored an expression for comparing dates

--- 07.IntroToSQLHomework/24.SalesAndFinanceEmployees.sql
+++ 07.IntroToSQLHomework/24.SalesAndFinanceEmployees.sql
@@ -8,5 +8,4 @@ inner join Departments y
 on x.DepartmentID = y.DepartmentID
 
 where (y.Name = 'Sales' or y.Name = 'Finance') and
-	  (datediff(YY, x.HireDate, '2005-01-01 00:00:00') > 0 and 
-	   datediff(YY, x.HireDate, '1995-01-01 00:00:00') < 0);
+	  (x.HireDate between '1995-01-01' and '2005-01-01');
